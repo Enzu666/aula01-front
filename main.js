83ff304f17277b5d18eb9fdae39e6278b0a5733f
ENZO CREPALDI PEIXOTO <25203666@CFP127ALUNO.LOCAL>
2026-02-02 11:28:47 -0300

    função para mudar o background criada

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor () {
+   const cor = document.getElementById('cor').value
+   document.documentElement.style.setProperty('--cor-bg', cor)
+}
+
+botaoTrocarCor.addEventListener('click', trocarCor)
