b899c0757c2a6b04376bd2166ff694d18261ad0c
ENZO CREPALDI PEIXOTO <25203666@CFP127ALUNO.LOCAL>
2026-02-02 11:30:35 -0300

    adicionando palavras em portugues

--- main.js
+++ main.js
@@ -3,7 +3,20 @@
 const botaoTrocarCor = document.getElementById('trocar-cor')
 
 function trocarCor () {
-   const cor = document.getElementById('cor').value
+   let cor = document.getElementById('cor').value.trim().toLowerCase()
+
+   if (cor === 'amarelo') {
+       cor = 'yellow'
+   } else if (cor === 'azul') {
+       cor = 'blue'
+   } else if (cor === 'verde') {
+       cor = 'green'
+   } else if (cor === 'vermelho') {
+       cor = 'red'
+   } else if (cor === 'laranja') {
+       cor = 'orange'
+   }
+
    document.documentElement.style.setProperty('--cor-bg', cor)
 }
 
